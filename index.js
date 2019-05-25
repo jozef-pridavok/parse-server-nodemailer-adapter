0b30a5f48abb267d465c141f8fa07450ab92da0d
Jozef Prídavok <jozef@pridavok.sk>
2019-05-25 22:58:18 +0200

    Existing implementation.

---
+++ index.js
@@ -0,0 +1,41 @@
+var nodemailer = require('nodemailer');
+
+var NodemailerAdapter = nodemailerOptions => {
+  if (!nodemailerOptions || !nodemailerOptions.user || !nodemailerOptions.password || !nodemailerOptions.from) {
+    throw 'NodemailerAdapter requires an user name, password, and fromAddress.';
+  }
+
+  var sendMail = mail => {
+
+    let message = {
+        from: nodemailerOptions.from,
+        to: mail.to, subject: mail.subject,
+        text: mail.text
+    };
+    let smtpConfig = {
+        host: nodemailerOptions.host,
+        port: nodemailerOptions.port,
+        secure: nodemailerOptions.secure,
+        auth: {
+            user: nodemailerOptions.user,
+            pass: nodemailerOptions.password
+        }
+    };
+    let transporter = nodemailer.createTransport(smtpConfig);
+
+    return new Promise((resolve, reject) => {
+      transporter.sendMail(message, (error, info) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(info);
+      });
+    });
+  }
+
+  return Object.freeze({
+    sendMail: sendMail
+  });
+}
+
+module.exports = NodemailerAdapter;
